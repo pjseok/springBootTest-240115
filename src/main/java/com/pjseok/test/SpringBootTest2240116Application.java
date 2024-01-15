647dec125eeff27f82f51eb6d1ea89cc7f1084d1
pjseok <pjs7850@naver.com>
2024-01-15 11:30:21 +0900

    JSP 설정 완료

---
+++ src/main/java/com/pjseok/test/SpringBootTest2240116Application.java
@@ -0,0 +1,13 @@
+package com.pjseok.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest2240116Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest2240116Application.class, args);
+	}
+
+}
